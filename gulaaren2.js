159c6605938178fa35b869dcb35c5470e51d340f
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-05-20 23:30:54 +0700

    Delete gulaaren2.js

--- gulaaren2.js
+++
@@ -1,5 +0,0 @@
-var dataUrl = [
-  'http://achievablecpmrevenue.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
