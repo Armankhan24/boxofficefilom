86fd8285d3e91eed24ff5feaaf362578ff90f516
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-05-22 13:32:31 +0700

    Update gulaaren4.js

--- gulaaren4.js
+++ gulaaren4.js
@@ -1,5 +1,8 @@
 var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
-var dataUrl = ["https://dev-boxofficefilom24.pantheonsite.io/"];
+var dataUrl = [
+"https://dev-boxofficefilom24.pantheonsite.io/",
+"https://dev-hdfilm21.pantheonsite.io/"
+];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem + param;
