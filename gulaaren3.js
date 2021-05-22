af6528a27ef7296e3204643a27a7f04299b33cd6
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-05-22 13:35:08 +0700

    Update gulaaren3.js

--- gulaaren3.js
+++ gulaaren3.js
@@ -1,5 +1,6 @@
 var dataUrl = [
-  '//www.achievablecpmrevenue.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec'
+  '//www.achievablecpmrevenue.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec',
+  '//www.achievablecpmrevenue.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
