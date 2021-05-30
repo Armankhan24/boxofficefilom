2fef2567b59e1765478974d7edf98c69e617169e
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-05-30 19:01:49 +0700

    Delete gulaaren3.js

--- gulaaren3.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-  '//limelightcopper.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec',
-  '//limelightcopper.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
