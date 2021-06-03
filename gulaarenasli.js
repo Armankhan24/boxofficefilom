cb1fdd6716aee625b5aa417968da79b598425fe7
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-06-03 14:40:51 +0700

    Delete gulaarenasli.js

--- gulaarenasli.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-  '//adorableold.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec',
-  '//adorableold.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
