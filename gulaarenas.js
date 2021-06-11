8f8e2ec4934f9ebb2050bb81b297ccd2860f95a0
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-06-11 20:38:31 +0700

    Delete gulaarenas.js

--- gulaarenas.js
+++
@@ -1,5 +0,0 @@
-var dataUrl = [
-  'https://adorableold.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
