7dfc1eb4d8b31e7698d42524824f958fc0fc16fc
Armankhan24 <84512758+Armankhan24@users.noreply.github.com>
2021-07-09 08:51:17 +0700

    Delete gulaarensvj.js

--- gulaarensvj.js
+++
@@ -1,6 +0,0 @@
-var dataUrl = [
-  'https://adorableold.com/psxtrb0ac?key=26b36460ba330937e712e871b14c6d05',
-  'https://adorableold.com/mwq4su0t?key=35a6f3c9cc4b4ad4de4d95065170e1ec'
-];
-var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
-window.location = randomItem;
